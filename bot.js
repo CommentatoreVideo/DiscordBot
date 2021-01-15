c9e9ca0fdf630340806114b7a04ae78072af8dd9
CommentatoreVideo <giangorn12345678@gmail.com>
2021-01-15 21:31:51 +0100

    Comandi !corsi e !corso

--- bot.js
+++ bot.js
@@ -1,20 +1,61 @@
 console.log("Beep beep i'm a sheep!");
 
 require("dotenv").config();
+const https = require("https");
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const regexCorso = /!corso (\d+)/;
+
 client.login(process.env.BOT_TOKEN);
 
-client.on("ready", ()=>{
+client.on("ready", () => {
   console.log("I like trains!");
 });
 
-client.on("message",msg=>{
-  console.log(msg.content);
+client.on("message", msg => {
   //if(msg.channel.id==idDelCanale) // Limita il canale
-  if(msg.content.toLowerCase() == "!ping") {
+  if(msg.content.toLowerCase() == "!ping")
     msg.reply("Pong!");
+  else if(msg.content.toLowerCase() == "!classificau")
+    msg.reply("https://www.umanet.net/students/ranking/2");
+  else if(msg.content.toLowerCase() == "!spoonriver")
+    msg.reply("Schifo totale! :face_vomiting:");
+  else if(msg.content.toLowerCase() == "!corsi") {
+    let testo = "";
+    https.get("https://www.umanet.net/api/subjects/", res => {
+      res.on('data', d => testo += d);
+      res.on("end", () => {
+        testo = eval(testo);
+        let ris = "I corsi sono:\n";
+        for(let i = 0; i < testo.length; i++)
+          ris += testo[i].title + "\n";
+        msg.reply(ris);
+      });
+    });
+  } else if(msg.content.toLowerCase().match(regexCorso)) {
+    const id = regexCorso.exec(msg.content.toLowerCase())[1];
+    let testo = "";
+    https.get("https://www.umanet.net/api/courses/", res => {
+      res.on('data', (d) => testo += d);
+      res.on("end", () => {
+        testo = eval(testo);
+        let ris = "Corso ";
+        for(let i = 0; i < testo.length; i++)
+          if(testo[i].id == id) {
+            ris += testo[i].title + ":\n";
+            ris+="Paragrafi:\n";
+            for(let j=0; j<testo[i].modules.length; j++) {
+              ris+="--"+testo[i].modules[j].title+"\n";
+              if(testo[i].modules[j].description)
+                ris+="----"+testo[i].modules[j].description+"\n";
+            }
+          }
+        msg.reply(ris);
+      });
+    });
   }
 });
+
+
